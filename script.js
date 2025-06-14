cfc156a0428b669efae57d18335dd551ec1d8bd3
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 13:53:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	 if (str == null) return []; // Handle null input
+
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  return result;
 }
 
 // Do not change the code below
